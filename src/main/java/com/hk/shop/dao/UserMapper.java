32fffd5ec4c72579a5ac2fa4b7dbc23345044f6a
HK <1404257522@qq.com>
2018-10-29 10:40:56 +0800

    dao层

---
+++ src/main/java/com/hk/shop/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.hk.shop.dao;
+
+import com.hk.shop.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
